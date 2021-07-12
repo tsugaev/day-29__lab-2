e91577dd73a4fe6fc38d0bef6e43d1c53758b994
vitarg <vitrigovm@gmail.com>
2021-07-12 15:32:50 +0300

    завершил release-1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const toDoList = [
+    {
+        text: 'Новое дело 1',
+        done: false
+    },
+    {
+        text: 'Новое дело 2',
+        done: false
+    },
+    {
+        text: 'Новое дело 3',
+        done: false
+    },
+    {
+        text: 'Новое дело 2',
+        done: false
+    },
+    {
+        text: 'Новое дело 3',
+        done: false
+    }
+]
